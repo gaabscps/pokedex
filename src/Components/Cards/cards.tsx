4bc0e8487fc177569a88d3c880ceeb4273d14ef6
gaabscps <gaabscps@gmail.com>
2022-08-02 15:10:45 -0300

    :sparkles: API connect working
    add api connect to both pokemon list and each pokemon individually

--- src/Components/Cards/cards.tsx
+++ src/Components/Cards/cards.tsx
@@ -1,19 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import api from '../../services/apiConfig';
 
 
-export interface CardProps{ 
-    name:string;
+export interface CardProps {
+    name: string;
 }
 
-function Cards({name}:CardProps) {
+function Cards({ name }: CardProps) {
+
+    const [imagem, setImagem] = useState<any>();
+    const [id, setId] = useState<any>();
+
+    useEffect(() => {
+        api.get("pokemon/" + name)
+            .then((response) => {
+                const { data } = response;
+                setImagem(data.sprites.front_default);
+                setId(data.id);
+            }
+            )
+            .catch((error) => console.log(error))
+    }, [])
+
     return (
         <Card className="card__ind" style={{ width: '15rem' }}>
-            <Card.Img variant="top" src="" />
+            <Card.Img variant="top" src={imagem} />
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>
+                    #{+ id}
+                </Card.Text>
+                <Card.Text>
+                    dasda
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -4,42 +4,49 @@ import Cards from '../Components/Cards/cards';
 import '../Components/Cards/cards.css'
 
 
+type pokemon = {
+    name: string;
+    front_default: string;
+}
+
 export default function Home() {
     const [pokemon, setPokemon] = useState<any[]>([]);
     const [pokemons, setPokemons] = useState<any[]>([]);
 
     useEffect(() => {
         getPokemons();
-        getPokemon();
+        // getPokemon();
     }, [])
 
     // useEffect(() => {
     //     getPokemon();
     // }, [pokemons])
 
-    function getPokemon() {
-        let caca: any[] = [];
-
-        for (let index = 0; index < pokemons.length; index++) {
-            const name = pokemons[index].name
-            api.get("pokemon/" + name)
-                .then(response => caca.push(response.data))
-        }
-        setPokemon(caca);
-    }
-
     function getPokemons() {
-        api.get("pokemon/?limit=10")
+        api.get("pokemon/?limit=40")
             .then((response) => setPokemons(response.data.results))
             .catch((error) => console.log(error))
     }
+
+    // function getPokemon() {
+    //     for (let index = 0; index < pokemons.length; index++) {
+    //             const name = pokemons[index].name;
+
+
+    //     }
+    // }
     return (
-        <div className="cards__container">
-            {pokemons.map((poke, index) => (
-                <Cards
-                    name={poke.name}
-                />
-            ))}
-        </div>
+        <>
+            <div className="cards__container">
+                {pokemons.map((poke, index) => (
+                    <Cards
+                        name={poke.name}
+                    />
+                ))}
+            </div>
+        </>
+
     )
 }
+
+
